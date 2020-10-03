b96f3e957eaa5bc295318a91744a0a8a582feb21
Cody Bonsma <codybonsma@gmail.com>
2020-10-02 21:23:28 -0400

    made changes to the paths to get rid of errors

--- server.js
+++ server.js
@@ -11,22 +11,23 @@ const app = express(); // this variable (app) points toward the value of the exp
 const PORT = process.env.PORT || 3000; // process.env listens to the environment for PORT connections
 
 // middleware to handle data parsing
-app.use(express.static("public"));
+app.use(express.static(path.join("./Develop/public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// app.use(express.static("public"));
 
 // Routes
 app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
 });
 
 app.get("/notes", function(req, res){
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
+    res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
 });
 
 app.get("/api/notes", function(req, res){
 console.log("testing this route");
-    fs.readFile("./db/db.json", function(err, data){
+    fs.readFile("./Develop/db/db.json", function(err, data){
         if(err) throw err;
         console.log(JSON.parse(data));
         let parsedData = JSON.parse(data);
@@ -37,13 +38,13 @@ console.log("testing this route");
 // use the POST method to handle the users request and store the users input into the db.json and page
 app.post("/api/notes", function(req, res){
     console.log(req.body);
-    fs.readFile("./db/db.json", "utf-8", function(err, data){
+    fs.readFile("./Develop/db/db.json", "utf-8", function(err, data){
         if(err) throw err;
         const parsedData = JSON.parse(data);
         req.body.id = uuidv4();
         parsedData.push(req.body);
 
-        fs.writeFile("./db/db.json", JSON.stringify(parsedData), "utf-8", (err) => {
+        fs.writeFile("./Develop/db/db.json", JSON.stringify(parsedData), "utf-8", (err) => {
             if (err) throw err;
         });
         res.json(parsedData);
@@ -51,13 +52,13 @@ app.post("/api/notes", function(req, res){
 })
 // DELETE route on db json notes
 app.delete("/api/notes/:id", function(req,res){
-    fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    fs.readFile("./Develop/db/db.json", "utf-8", (err, data) => {
         if (err) throw err;
         const pickled = JSON.parse(data);
         parsedData = pickled.filter((data) => {
             return data.id != req.params.id
         })
-        fs.writeFile("./db/db.json", JSON.stringify(parsedData), (err) => {
+        fs.writeFile("./Develop/db/db.json", JSON.stringify(parsedData), (err) => {
             if (err) throw err;
             res.json(parsedData);
         });
